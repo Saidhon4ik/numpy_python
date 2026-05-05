a28a518a2c5ff46d63d00c0a87ea8bf87ccac36e
Saidhon4ik <saidhonabdullaev@gmail.com>
2026-05-05 18:15:37 +0500

    making it look better

--- src/filtering.py
+++ src/filtering.py
@@ -1,23 +1,35 @@
+#===============================================================================#
 #Filtering in numpy = Refers to the proccess of selecting elements from an array
 #that matches given condition
 #Sort of a 'Where' thing from SQL but in numpy python
+#===============================================================================#
 import numpy as np
 
 ages = np.array([[21,17,19,20,16,30,18,65],
                  [39,22,15,99,18,19,20,21]])
 
+
+
+#======================================#
+#our arrays
 teens = ages[ages < 18]
 adults1 = ages[(ages<=18) & (ages<65)]
 seniors = ages[ages>=65]
 evens = ages[ages%2 == 0]
 odds = ages[ages%2 != 0]
+#======================================#
 
 
+
+#==============#
+#calling all functions
 print(teens)
 print(adults1)
 print(seniors)
 print(evens)
 print(odds)
+#==============#
+
 
 
 #=================================================================================================#
