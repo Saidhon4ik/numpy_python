55b6d09f1b3ea626eb53fddc2286ecb3a08f8e02
Saidhon4ik <saidhonabdullaev@gmail.com>
2026-05-05 18:12:43 +0500

    making it look better

--- src/aggregate_funcs.py
+++ src/aggregate_funcs.py
@@ -4,7 +4,8 @@ import numpy as np
 arr = np.array([[1, 2, 3, 4, 5],
                 [6, 7, 8, 9, 10]])
 
-
+#===============================================#
+#Basic ones
 print(np.sum(arr))
 print(np.mean(arr))
 print(np.std(arr)) #statistic thing
@@ -13,6 +14,6 @@ print(np.min(arr))
 print(np.max(arr))
 print(np.argmin(arr)) #index of a minimum number
 print(np.argmax(arr)) #index of a maxumum number
-
+#================================================#
 
 print(np.sum(arr,axis = 1)) #we are summing all columns | axis = 0 sum of all columns | axis = 1 sum of all rows
