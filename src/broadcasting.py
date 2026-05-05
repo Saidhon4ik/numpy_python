c4eca0a15d59468e4a6d515881c43b9003323463
Saidhon4ik <saidhonabdullaev@gmail.com>
2026-05-05 17:33:06 +0500

    broadcasting in python numpy

---
+++ src/broadcasting.py
@@ -0,0 +1,55 @@
+#Broadcasting allows NumPy to perform operations on arrays
+#with different shapes and virtually expanding dimensions
+#so they match larger array's shape
+
+
+#The dimensions have the same size
+#OR
+#One of the dimensions has a size of 1
+
+
+#(10,1) * (1,10)    --> 👍
+#(10,10) * (10,1)   --> 👍
+#(10,10) * (10,10 ) --> 👍
+
+import numpy as np
+arr1 = np.array([[1],
+                 [2],
+                 [3],
+                 [4]])
+arr2 = np.array([[1,2,3,4],
+                 [5,6,7,8],
+                 [9,10,11,12],
+                 [13,14,15,16]])
+arr3 = np.array([[1,2,3,4]])
+
+#just checking their shapes
+print(arr1.shape)
+print(arr2.shape)
+
+#mul
+print(arr1 * arr2)
+print(arr1 * arr3)
+
+
+
+
+
+
+array1 = np.array([[1,2,3,4,5,6,7,8,9,10]])
+array2 = np.array([[1],
+                   [2],
+                   [3],
+                   [4],
+                   [5],
+                   [6],
+                   [7],
+                   [8],
+                   [9],
+                   [10],])
+
+
+print(array1.shape)
+print(array2.shape)
+
+print(array1*array2)
