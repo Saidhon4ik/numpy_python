6f0225092eb090a410ebe91e1a0ed9de1f1695d7
Saidhon4ik <saidhonabdullaev@gmail.com>
2026-05-05 18:18:53 +0500

    better looking

--- src/broadcasting.py
+++ src/broadcasting.py
@@ -1,3 +1,4 @@
+#===========================================================#
 #Broadcasting allows NumPy to perform operations on arrays
 #with different shapes and virtually expanding dimensions
 #so they match larger array's shape
@@ -11,7 +12,11 @@
 #(10,1) * (1,10)    --> 👍
 #(10,10) * (10,1)   --> 👍
 #(10,10) * (10,10 ) --> 👍
+#===========================================================#
 
+
+
+#================================#
 import numpy as np
 arr1 = np.array([[1],
                  [2],
@@ -22,20 +27,26 @@ arr2 = np.array([[1,2,3,4],
                  [9,10,11,12],
                  [13,14,15,16]])
 arr3 = np.array([[1,2,3,4]])
+#================================#
 
+
+
+#===========================#
 #just checking their shapes
 print(arr1.shape)
 print(arr2.shape)
+#===========================#
 
+
+#===========================#
 #mul
 print(arr1 * arr2)
 print(arr1 * arr3)
+#===========================#
 
 
 
-
-
-
+#=============================================#
 array1 = np.array([[1,2,3,4,5,6,7,8,9,10]])
 array2 = np.array([[1],
                    [2],
@@ -47,9 +58,11 @@ array2 = np.array([[1],
                    [8],
                    [9],
                    [10],])
+#=============================================#
 
 
+#====================#
 print(array1.shape)
 print(array2.shape)
-
 print(array1*array2)
+#====================#
